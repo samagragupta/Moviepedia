ddf99693f97ffe7456353da12f09f0027c61840e
samagragupta <samagraguptajpr@gmail.com>
2019-01-18 16:12:18 +0530

    fixed page reload issue and added child route

--- moviepedia/src/app/app-routing.module.ts
+++ moviepedia/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { AllMoviesComponent } from './all-movies/all-movies.component';
+import { MoviesDetailComponent } from './movies/movies-detail/movies-detail.component';
 
 const appRoutes: Routes = [
-    {path: 'movies', component: MoviesComponent},
+    {path: 'movies', component: MoviesComponent, children: [
+        {path: ':id',component: MoviesDetailComponent}
+    ]},
     {path: 'movielist', component: AllMoviesComponent},
 ]
 
