7857b59238ab1adc47b3a64bec1bab3f5936e97c
samagragupta <samagraguptajpr@gmail.com>
2019-01-22 10:00:23 +0530

    Added search button option with functionality

--- moviepedia/src/app/all-movies/all-movies.component.ts
+++ moviepedia/src/app/all-movies/all-movies.component.ts
@@ -10,8 +10,10 @@ import { AllMoviesService } from './all-movies.service';
 export class AllMoviesComponent implements OnInit {
 
   movies: any;
+  movie: any;
   upcoming_movies: any;
   popular_movies: any;
+  search_result: any;
 
   constructor(public allMovieService: AllMoviesService) { 
     this.movies = [
@@ -29,6 +31,12 @@ export class AllMoviesComponent implements OnInit {
       this.popular_movies = data['results'];
     });
 
+  }
+
+  searchMovies() {
+    this.allMovieService.searchMovie(this.movie).subscribe(data => {
+      this.search_result = data['results'];
+    });
   }
 
   ngOnInit() {}
--- moviepedia/src/app/all-movies/all-movies.service.ts
+++ moviepedia/src/app/all-movies/all-movies.service.ts
@@ -8,6 +8,8 @@ export class AllMoviesService {
 
   private movie_url = 'https://api.themoviedb.org/3/';
   private api_key = '4a7711ce4033e38e64bb7adb7c50cff2';
+  private movie_string: any;
+  movie: any;
 
   constructor(public _http: HttpClient) { }
 
@@ -17,5 +19,10 @@ export class AllMoviesService {
 
   getPopularMovies() {
     return this._http.get(this.movie_url + 'discover/movie?sort_by=popularity.desc' + '&api_key=' + this.api_key);
+  }
+
+  searchMovie(movie: string) {
+    this.movie_string = movie;
+    return this._http.get(this.movie_url + 'search/movie?query=' + this.movie_string + '&api_key=' + this.api_key);
   }
 }
